500300ef1886d0058b6c4fa8b78922c67d348885
I <wzulfikar@ymail.com>
2016-10-16 16:53:37 +0800

    make fetcher as student's method

--- main.go
+++ main.go
@@ -11,66 +11,15 @@ http://website.com/upser_profile_page .
 package main
 
 import (
-	// "code.google.com/p/go.net/publicsuffix"
-	"io/ioutil"
+	"github.com/wzulfikar/iium/student"
 	"log"
-	"net/http"
-	// "net/http/cookiejar"
-	"net/url"
 )
 
-type StudentCred struct {
-	matric string
-	pwd    string
-}
-
-func fetch(END_POINT string, PARAMS url.Values) string {
-	client := http.Client{}
-
-	resp, err := client.PostForm(END_POINT, PARAMS)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	data, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	// return whole html of user profile data
-	return string(data)
-}
-
-func FetchResult(sc *StudentCred) {
-	const END_POINT string = "http://prereg.iium.edu.my/resslip/viewResult.php"
-	PARAMS := url.Values{
-		"mat_no":   {sc.matric},
-		"pin_no":   {sc.pwd},
-		"sessi":    {"2015/2016"},
-		"semester": {"3"},
-		"login":    {"Login"},
-	}
-	log.Println(fetch(END_POINT, PARAMS))
-}
-
-func FetchCam(sc *StudentCred) {
-	const END_POINT string = "http://albiruni.iium.edu.my/myapps/StudentOnline/camResult1.php"
-	PARAMS := url.Values{
-		"mat_no":   {sc.matric},
-		"pin":      {sc.pwd},
-		"sessi":    {"2015/2016"},
-		"semester": {"3"},
-		"login":    {"login"},
-		"action":   {"view"},
-	}
-	log.Println(fetch(END_POINT, PARAMS))
-}
-
 func main() {
-	sc := StudentCred{
+	s := student.Cred{
 		"1222665",
 		"Wzulfikar031",
 	}
-	FetchCam(&sc)
-	FetchResult(&sc)
+	log.Println(s.GetResult())
+	log.Println(s.GetCam())
 }
